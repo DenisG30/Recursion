92b0d9cc575a5e453190af6b3b130642f06c344b
DenisG30 <gorbunowdenis30@gmail.com>
2025-07-02 11:11:14 +0400

    Create Task1.java

---
+++ Task1.java
@@ -0,0 +1,21 @@
+public class Task1 {
+    public static double Exponentiation(double x, int y) {
+        if (x == 0 && y <= 0){
+            throw new ArithmeticException("Невозможно рассчитать");
+        }
+        
+        if (x == 1 || x == 0) {
+            return x;
+        }
+
+        if (y > 1 ) {
+            return x * Exponentiation(x, --y);
+        }
+
+        if (y < 1 ) {
+            return 1 / x * Exponentiation(x, ++y);
+        }
+        
+        return x;
+    }
+}
