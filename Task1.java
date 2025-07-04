c2ed8c9184c4b443b4392e0968148cb9f76b6cc0
DenisG30 <gorbunowdenis30@gmail.com>
2025-07-04 14:46:10 +0400

    Update Task.java

--- Task1.java
+++ Task1.java
@@ -3,30 +3,35 @@ public class Task1 {
         if (x == 0 && y <= 0){
             throw new ArithmeticException("Невозможно рассчитать");
         }
-        
         if (x == 1 || x == 0) {
             return x;
         }
-
         if (y > 1 ) {
             return x * Exponentiation(x, --y);
         }
-
         if (y < 1 ) {
             return 1 / x * Exponentiation(x, ++y);
         }
-        
         return x;
     }
 
+    
     public static int SumOfDigit(int x){
         if (x < 0){
             throw new ArithmeticException("Число меньше 0");
         }
-        
         if(x < 10 && x >= 0) {
             return x;
         } 
         return (x % 10 + SumOfDigit(x / 10));
+    }
+
+    
+    public static int lengthList(ArrayList<?> list){
+        if (list.isEmpty()) {
+            return 0;
+        }
+        list.removeFirst();
+        return 1 + lengthList(list);
     }
 }
